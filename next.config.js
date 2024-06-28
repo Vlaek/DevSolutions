09963b9c2481179a4bb21a9253ee25adbe8356ac
Vladislav Eichwald <vlad.eichwald@gmail.com>
2024-06-28 22:42:48 +0700

    try deploy

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const createNextIntlPlugin = require('next-intl/plugin')
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  distDir: '/dist',
-}
+const nextConfig = {}
 
 module.exports = withNextIntl(nextConfig)
